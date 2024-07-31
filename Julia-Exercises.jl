850f2310c364eed151d6b392cc054445f72d2c4e
Hectorarnandez <hectorarnandez@gmail.com>
2024-07-31 11:53:26 -0600

    New_Examples

--- Julia-Exercises.jl
+++ Julia-Exercises.jl
@@ -1,2 +1,7 @@
-parse(Int64, "1")
+# get component example
+
+using PowerSystems
+file_dir = joinpath(pkgdir(PowerSystems), "docs", "src", "tutorials", "tutorials_data")
+system = System(joinpath(file_dir, "RTS_GMLC.m"));
+thermal_gens = get_components(ThermalStandard, system)
 
